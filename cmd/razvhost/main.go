aafa36bffdda3e54cba210021da5fdf451a564ea
Gábor Görzsöny <gabor@gorzsony.com>
2020-10-08 13:00:06 +0200

    Added command line arg to configure certs dir

--- cmd/razvhost/main.go
+++ cmd/razvhost/main.go
@@ -6,11 +6,13 @@ import (
 
 // command line args
 var (
+	CertsDir          string
 	WatchDockerEvents bool
 	EnableHTTP2       bool
 )
 
 func init() {
+	flag.StringVar(&CertsDir, "certs", "certs", "Directory to store certificates in")
 	flag.BoolVar(&WatchDockerEvents, "docker", false, "Watch Docker events to find containers with VIRTUAL_HOST")
 	flag.BoolVar(&EnableHTTP2, "http2", false, "Enable HTTP2 requests towards internal ports")
 	flag.Parse()
@@ -18,7 +20,7 @@ func init() {
 
 func main() {
 	cfg := &ServerConfig{
-		CertsDir:          "certs",
+		CertsDir:          CertsDir,
 		WatchDockerEvents: WatchDockerEvents,
 		EnableHTTP2:       EnableHTTP2,
 	}
